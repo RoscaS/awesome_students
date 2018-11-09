73030a5070005c5af6c8367b28be1cacff6f98db
roscas <sol.rosca@he-arc.ch>
2018-11-09 01:21:35 +0100

    bdd

---
+++ .vuepress/components/GOL/Controller.vue
@@ -0,0 +1,129 @@
+<template>
+
+  <div class="box">
+    <div class="field has-addons has-addons-centered is-marginless">
+      <!--<p class="control">-->
+        <button :disabled="mainComponent !== 'gamePage'"
+                class="button"
+                title="slow down"
+                @click="send('slowDown')">
+          <i class="fas fa-backward"></i>
+        </button>
+      <!--</p>-->
+      <!--<p class="control">-->
+        <button :disabled="mainComponent !== 'gamePage'"
+                :class="isRunning ? 'green' : 'red'"
+                class="button"
+                title="play"
+                @click="send('play')">
+          <i :class="isRunning ? 'fa-pause-circle' : 'fa-play-circle'"
+             class="far"></i>
+        </button>
+      <!--</p>-->
+      <!--<p class="control">-->
+        <button :disabled="mainComponent !== 'gamePage'"
+                class="button"
+                title="clear grid"
+                @click="send('redoSession')">
+          <i class="fas fa-redo"></i>
+        </button>
+      <!--</p>-->
+      <!--<p class="control">-->
+        <button :disabled="mainComponent !== 'gamePage'"
+                class="button"
+                title="next step"
+                @click="send('nextStep')">
+          <i class="fas fa-step-forward"></i>
+        </button>
+      <!--</p>-->
+      <!--<p class="control">-->
+        <button :disabled="mainComponent !== 'gamePage'"
+                class="button"
+                title="speed up"
+                @click="send('speedUp')">
+          <i class="fas fa-forward"></i>
+        </button>
+      <!--</p>-->
+      <!--<p class="control">-->
+        <button :disabled="mainComponent !== 'gamePage'"
+                class="button"
+                @click="send('randomSeed')">
+          <span class="icon">
+            <i class="fas fa-random"></i>
+          </span>
+          <b>Random</b>
+        </button>
+      <!--</p>-->
+    </div>
+    <!--<div class="field has-addons has-addons-centered is-marginless">-->
+      <!--<p class="control">-->
+        <!--<button-->
+                <!--:disabled="mainComponent !== 'gamePage'"-->
+                <!--class="button"-->
+                <!--@click="send('importSession')">-->
+          <!--<span class="icon">-->
+            <!--<i class="far fa-edit"></i>-->
+          <!--</span>-->
+          <!--<b>Import</b>-->
+        <!--</button>-->
+      <!--</p>-->
+      <!--<p class="control">-->
+        <!--<button-->
+                <!--:disabled="mainComponent !== 'gamePage'"-->
+                <!--class="button"-->
+                <!--@click="send('exportSession')">-->
+          <!--<span class="icon">-->
+            <!--<i class="far fa-save"></i>-->
+          <!--</span>-->
+          <!--<b>Export</b>-->
+        <!--</button>-->
+      <!--</p>-->
+    <!--</div>-->
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: {
+      isRunning: {
+        default: false,
+        type: Boolean,
+      },
+      mainComponent: {
+        default: 'gamePage',
+        type: String,
+      },
+    },
+    data() {
+      return {};
+    },
+    methods: {
+      /**
+       * emits the action that is requested
+       * by the client up to the App component.
+       *
+       * @param {string} event - the event
+       */
+      send: function (event) {
+        this.$emit('send', event);
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .box {
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .green {
+    background-color: hsla(120, 77%, 26%, 0.6);
+  }
+
+  .red {
+    background-color: hsla(0, 67%, 31%, 0.6);
+  }
+</style>
---
+++ projets/p2/00_gol-test.md
@@ -0,0 +1,9 @@
+---
+title: GOL Test
+date: 2018-10-02
+author: Sol
+sidebar: auto
+project: false
+---
+
+<GameOfLife/>
